3d926978b9a656ce6c6938f4e374d34c0b29e84c
JRL1004 <Loosa.J.History@gmail.com>
2015-02-06 20:36:39 -0700

    Imported the Configuration class from the original FuseBox as well as the MySQL database tools. Added full Javadoc support for MySQL classes.

--- src/mc/lightcraft/java/FuseBox.java
+++ src/mc/lightcraft/java/FuseBox.java
@@ -6,18 +6,27 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class FuseBox extends JavaPlugin {
 	private static FuseBox instance;
 	
-	
+	/**
+	 * Called when FuseBox initiates. Creates the active instance and registers any needed information
+	 */
 	public void onEnable() {
 		instance = this;
 		super.onEnable();
 	}
 
+	/**
+	 * This function is called when FuseBox is disabled. Clears the active instance and unregisters all event for this plugin
+	 */
 	public void onDisable() {
 		HandlerList.unregisterAll(instance);
 		instance = null;
 		super.onDisable();
 	}
 	
+	/**
+	 * This will get the active instance of FuseBox
+	 * @return active FuseeBox instance
+	 */
 	public static FuseBox getFuseBox() {
 		return instance;
 	}
