a0f83de4e2bddaf2360eb48d5b1ce2e11c606d75
ArrayPro <arraypro07@gmail.com>
2015-02-10 16:10:52 -0500

    Added rank tree skeleton and fixed plugin.yml

---
+++ src/mc/lightcraft/java/common/rank/tree/RankTree.java
@@ -0,0 +1,95 @@
+package mc.lightcraft.java.common.rank.tree;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+import org.bukkit.OfflinePlayer;
+
+public class RankTree {
+
+	private static RankTree tree;
+	private HashMap<UUID, ServerRank> ram = new HashMap<UUID, ServerRank>();
+
+	/**
+	 * RankTree constructor is private; we only want one instance.
+	 */
+	private RankTree() {
+	}
+
+	/**
+	 * Get the instance of the rank tree.
+	 */
+	public static RankTree getTree() {
+		if (tree == null) {
+			tree = new RankTree();
+		}
+		return tree;
+	}
+
+	/**
+	 * Simple method to get the tree list in order. Use this instead of .values,
+	 * because we can change it later.
+	 */
+	public ServerRank[] getTreeList() {
+		return ServerRank.values();
+	}
+
+	/**
+	 * Check if a player has permission for a rank in the tree.
+	 * 
+	 * @param player
+	 *            The rank the player/checking object is
+	 * @param r
+	 *            The permission's rank/checking against
+	 */
+	public static boolean hasRank(ServerRank player, ServerRank r) {
+		if (player.toString().equalsIgnoreCase(r.toString()))
+			return true;
+		for (ServerRank r5 : ServerRank.values()) {
+			if (r5.toString().equalsIgnoreCase(player.toString()))
+				return false;
+			if (r5.toString().equalsIgnoreCase(r.toString()))
+				return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Set a player's rank in RAM, NOT on the database.
+	 * 
+	 * @param p
+	 *            The player's rank you would like to load into RAM.
+	 * @param r
+	 *            The rank you would like to load into RAM.
+	 */
+	public void setRank(OfflinePlayer p, ServerRank r) {
+		if (ram.containsKey(p.getUniqueId())) {
+			ram.remove(p.getUniqueId());
+		}
+		ram.put(p.getUniqueId(), r);
+	}
+
+	/**
+	 * Get a player's rank in RAM, NOT on the database.
+	 * 
+	 * @param p
+	 *            The player you would like to read from RAM.
+	 */
+	public ServerRank getRank(OfflinePlayer p) {
+		if (ram.containsKey(p.getUniqueId())) {
+			return ram.get(p.getUniqueId());
+		}
+		return ServerRank.Default;
+	}
+
+	/**
+	 * Unload a player's rank in RAM, NOT on the database.
+	 * 
+	 * @param p
+	 *            The player you would like to unload from RAM.
+	 */
+	public void unloadRank(OfflinePlayer p) {
+		ram.remove(p.getUniqueId());
+	}
+
+}
---
+++ src/mc/lightcraft/java/common/rank/tree/ServerRank.java
@@ -0,0 +1,7 @@
+package mc.lightcraft.java.common.rank.tree;
+
+public enum ServerRank {
+
+	Default, VIP, Blaze, Aurora, Helper, Mod, Admin, Developer, BuildTeam, Owner, CoOwner;
+
+}
